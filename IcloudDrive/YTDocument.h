7eecee6bd7bf9bff37e8a96f38e6923d2db98b24
Baron_Nash <745352049@qq.com>
2018-09-08 17:26:32 +0800

    commit

---
+++ IcloudDrive/YTDocument.h
@@ -0,0 +1,13 @@
+//
+//  YTDocument.h
+//  IcloudDrive
+//
+//  Created by 水晶岛 on 2018/9/8.
+//  Copyright © 2018年 水晶岛. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface YTDocument : UIDocument
+
+@end
---
+++ IcloudDrive/iCloudManager.h
@@ -0,0 +1,13 @@
+//
+//  iCloudManager.h
+//  IcloudDrive
+//
+//  Created by 水晶岛 on 2018/9/8.
+//  Copyright © 2018年 水晶岛. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface iCloudManager : NSObject
+
+@end
